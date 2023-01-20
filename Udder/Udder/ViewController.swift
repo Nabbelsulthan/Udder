fde0964351081d3acc574515b9f49e6dd22138b3
Nabbelsulthan <123071611+Nabbelsulthan@users.noreply.github.com>
2023-01-20 09:40:00 +0530

    Add files via upload
    git adding

---
+++ Udder/Udder/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Thippu Sulthan on 20/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        //
+    }
+}
+
